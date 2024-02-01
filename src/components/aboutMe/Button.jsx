5633f59175f81281977f74ce9e63bf091c6ac8e8
JUAN DAVID GUTIERREZ GUTIERREZ <dg427938@gmail.com>
2024-01-31 21:57:16 -0600

    add button for download cv

--- src/components/aboutMe/Button.jsx
+++ src/components/aboutMe/Button.jsx
@@ -4,7 +4,9 @@
   return (
     <div
         className="hover:bg-[#6200AD] bg-[#7E00DE] text-white mt-6 w-36 p-2 rounded-md flex justify-center items-center cursor-pointer"
-    >Descargar cv</div>
+    >
+    <a download="DavidGutierrezCV" href="download/DavidGutierrez.pdf">Descargar cv</a>
+    </div>
   )
 }
 
